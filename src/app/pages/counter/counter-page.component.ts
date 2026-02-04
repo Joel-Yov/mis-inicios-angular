a043081149a1c8968b38c0ca03303bfb270029a7
Joel Yovera <ingyovera6114@gmail.com>
2026-02-04 11:34:57 -0500

    feat(signals): introduction about UPDATE and SET for SIGNALS

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
     templateUrl: `./counter-page.component.html`,
@@ -6,18 +6,24 @@ import { Component } from "@angular/core";
 })
 export class CounterPageComponent {
     counter = 21;
+    counterSignal = signal(10);
 
     increaseBy(value: number): void
     {
         this.counter += value
+
+        //Usamos el current para obtener el valor actual de la señal
+        this.counterSignal.update((current) => current + value);
     }
 
     resetCounter(): void {
-        this.counter = 21;
+        this.counter = 0;
+        this.counterSignal.set(0); //Usamos el set para establecerle el valor que está entre ()
     }
 
     decreaseBy(value: number): void 
     {
         this.counter -= value
+        this.counterSignal.update((current) => current - value)
     }
 }
